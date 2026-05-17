36813e2acfbccc9c7592bb3e727643cf47757a28
Luke Craig <lacraig3@gmail.com>
2026-05-17 18:55:11 -0400

    penguin: add native mmap aperture mmio hooks

--- system/penguin.c
+++ system/penguin.c
@@ -1,5 +1,16 @@
 #include "qemu/osdep.h"
 #include "system/penguin.h"
+#include "system/address-spaces.h"
+#include "system/memory.h"
+
+typedef struct PenguinMmioRegion {
+    MemoryRegion mr;
+    MemoryRegionOps ops;
+    penguin_mmio_read_cb_t read_cb;
+    penguin_mmio_write_cb_t write_cb;
+    void *opaque;
+    char *name;
+} PenguinMmioRegion;
 
 static penguin_guest_hypercall_cb_t penguin_guest_hypercall_cb;
 static void *penguin_guest_hypercall_opaque;
@@ -37,4 +48,60 @@ bool penguin_handle_guest_hypercall(CPUState *cs, uint64_t nr,
     }
 
     return false;
+}
+
+static uint64_t penguin_mmio_read(void *opaque, hwaddr addr, unsigned size)
+{
+    PenguinMmioRegion *region = opaque;
+
+    if (!region->read_cb) {
+        return 0;
+    }
+
+    return region->read_cb(addr, size, region->opaque);
+}
+
+static void penguin_mmio_write(void *opaque, hwaddr addr, uint64_t data,
+                               unsigned size)
+{
+    PenguinMmioRegion *region = opaque;
+
+    if (!region->write_cb) {
+        return;
+    }
+
+    region->write_cb(addr, data, size, region->opaque);
+}
+
+int __attribute__((visibility("default")))
+penguin_qemu_add_mmio_region(uint64_t base, uint64_t size,
+                             const char *name,
+                             penguin_mmio_read_cb_t read_cb,
+                             penguin_mmio_write_cb_t write_cb,
+                             void *opaque)
+{
+    PenguinMmioRegion *region;
+
+    if (!size || !name || (!read_cb && !write_cb)) {
+        return -1;
+    }
+
+    region = g_new0(PenguinMmioRegion, 1);
+    region->read_cb = read_cb;
+    region->write_cb = write_cb;
+    region->opaque = opaque;
+    region->name = g_strdup(name);
+    region->ops.read = penguin_mmio_read;
+    region->ops.write = penguin_mmio_write;
+    region->ops.endianness = DEVICE_NATIVE_ENDIAN;
+    region->ops.valid.min_access_size = 1;
+    region->ops.valid.max_access_size = 8;
+    region->ops.impl.min_access_size = 1;
+    region->ops.impl.max_access_size = 8;
+
+    memory_region_init_io(&region->mr, NULL, &region->ops, region,
+                          region->name, size);
+    memory_region_add_subregion_overlap(get_system_memory(), base,
+                                        &region->mr, -1000);
+    return 0;
 }
